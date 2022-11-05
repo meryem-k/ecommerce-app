a5afb68aa4d8d38627ae9b31b84c6f488938940b
sule11 <smkargin11@gmail.com>
2022-11-05 17:35:39 -0500

    initial commit

---
+++ src/test/java/com/cydeo/ecommerceapp/EcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
