6492ed222e3576ba6a61b8ffeb02c2888ff6032f
sule11 <smkargin11@gmail.com>
2022-11-05 22:01:34 -0500

    added product and category tables

---
+++ src/main/java/com/cydeo/entity/Category.java
@@ -0,0 +1,17 @@
+package com.cydeo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Category extends BaseEntity {
+
+    private String name;
+
+
+}
---
+++ src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product extends BaseEntity {
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer remaining_quantity;
+
+
+    @ManyToMany
+    @JoinTable(name = "product_category_rel",
+                joinColumns = @JoinColumn(name = "p_id"),
+                inverseJoinColumns = @JoinColumn(name = "c_id"))
+    private List<Category> categoryList;
+
+}
